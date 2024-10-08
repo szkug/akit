12993e5a8fc5370cf0d293a575f532d99ff3865e
korilin <korilin.dev@gmail.com>
2024-10-08 17:31:09 +0800

    remove draw9patch func

--- app/src/main/java/com/korilin/samples/compose/trace/acts/NinePatchActivity.kt
+++ app/src/main/java/com/korilin/samples/compose/trace/acts/NinePatchActivity.kt
@@ -1,41 +1,16 @@
 package com.korilin.samples.compose.trace.acts
 
-import android.graphics.Rect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import com.korilin.samples.compose.trace.R
 import com.korilin.samples.compose.trace.glide.glideBackground
-
-fun Modifier.draw9Patch(
-    @DrawableRes ninePatchRes: Int,
-) = composed {
-    val context = LocalContext.current
-    drawBehind {
-        drawIntoCanvas {
-            val ninePatch = ContextCompat.getDrawable(context, ninePatchRes)!!
-            ninePatch.run {
-                bounds = Rect(0, 0, size.width.toInt(), size.height.toInt())
-                draw(it.nativeCanvas)
-            }
-        }
-    }
-}
 
 class NinePatchActivity : ComponentActivity() {
 
@@ -46,33 +21,6 @@ class NinePatchActivity : ComponentActivity() {
             Column(
                 modifier = Modifier.padding(10.dp)
             ) {
-
-                Text(
-                    text = "Kotlin",
-                    color = Color.White,
-                    modifier = Modifier.draw9Patch(
-                        ninePatchRes = R.drawable.chat_msg_bg,
-                    )
-                )
-
-
-                Text(
-                    text = "Kotlin & Compose & Kotlin & Compose",
-                    color = Color.White,
-                    modifier = Modifier.draw9Patch(
-                        ninePatchRes = R.drawable.chat_msg_bg,
-                    )
-                )
-
-
-                Text(
-                    text = "Kotlin & Compose & Kotlin & Compose & Kotlin & Compose & Kotlin & Compose & Kotlin & Compose",
-                    color = Color.White,
-                    modifier = Modifier.draw9Patch(
-                        ninePatchRes = R.drawable.chat_msg_bg,
-                    )
-                )
-
 
                 Text(
                     text = "Kotlin",
